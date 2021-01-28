3b9cf87a78df524ccbe306dd93ceaecb007e4600
gonzamarJM <gonzalo.marcos.regidor@iesjulianmarias.es>
2021-01-28 02:58:20 +0100

    1

--- Trivial/app/src/main/java/com/example/trivial/BaseDatosTrivial.java
+++ Trivial/app/src/main/java/com/example/trivial/BaseDatosTrivial.java
@@ -1,8 +1,14 @@
 package com.example.trivial;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.trivial.Estructurabd.CabecerasPregunta;
+import com.example.trivial.Estructurabd.CabecerasRespuesta;
+import com.example.trivial.Estructurabd.CabecerasPartida;
+import com.example.trivial.Estructurabd.CabecerasJugador;
 
 import androidx.annotation.Nullable;
 
@@ -21,13 +27,13 @@ public class BaseDatosTrivial  extends SQLiteOpenHelper {
     }
 
     interface Referencias {
-       /* String PREGUNTAS = String.format("REFERENCES %s(%s) ON DELETE CASCADE",
-                Tablas.CABECERA_PEDIDO, CabecerasPedido.ID_CABECERA_PEDIDO);
-        String RESPUESTAS;
+       String PREGUNTAS = String.format("REFERENCES %s(%s) ON DELETE CASCADE", Tablas.PREGUNTAS, CabecerasPregunta.ID);
+        String RESPUESTAS = String.format("REFERENCES %s(%s)", Tablas.RESPUESTAS, CabecerasRespuesta.ID);
 
-        String PARTIDAS;
-        String JUGADORES;*/
+        String PARTIDAS = String.format("REFERENCES %s(%s) ON DELETE CASCADE", Tablas.PARTIDAS, CabecerasPartida.ID);
+        String JUGADORES = String.format("REFERENCES %s(%s)", Tablas.JUGADORES, CabecerasJugador.ID);
     }
+
     public BaseDatosTrivial(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
