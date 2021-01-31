2309ca67eedc7e711918a10c16d375a708eea853
mohamed <mohaben124@gmail.com>
2021-01-31 22:58:01 +0100

    septimo commit

--- Trivial/app/src/main/java/com/example/trivial/MainActivity.java
+++ Trivial/app/src/main/java/com/example/trivial/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.trivial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -17,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     public static final Random RANDOM = new Random();
 
-    Button b_dado,b_ficha,b1;
+    Button b_dado,b_ficha1,b_ficha2,b1;
     ImageView iv_dado,q_azul,q_rojo,q_naranja,q_amarillo,q_verde,q_morado;
+    TextView t_j1,t_j2;
 
 
     private static final int[] CASILLAS = {
@@ -31,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     };
 
     Map<String,String> cMapa= new HashMap<String,String>();
+
+    int posFicha, turno = 0, tirar = 1 , n = 0;
+    String cFicha;
 
     private float posX,posY;
 
@@ -49,9 +56,12 @@ public class MainActivity extends AppCompatActivity {
         cMapa.put("m","va");
 
         b_dado = (Button)findViewById(R.id.b_dado);
-        b_ficha = (Button)findViewById(R.id.b_ficha);
+        b_ficha1 = (Button)findViewById(R.id.b_ficha1);
+        b_ficha2 = (Button)findViewById(R.id.b_ficha2);
         iv_dado = (ImageView)findViewById(R.id.iv_dado);
         q_azul= findViewById(R.id.quisitoA);
+        t_j1 = findViewById(R.id.tJugador);
+        t_j2 = findViewById(R.id.tJugador2);
 
         b_dado.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,88 +71,108 @@ public class MainActivity extends AppCompatActivity {
 
                 int nDado = RANDOM.nextInt(6)+1;
 
-                for(int id : CASILLAS) {
-                    final Button button = (Button) findViewById(id);
+                if(tirar == 1){
+                    n = nDado;
+                    t_j1.setTextColor(Color.GREEN);
+                    t_j2.setTextColor(Color.RED);
+                    tirar++;
 
-                    // numero de la posicion de la casilla
-                    int posCasilla = button.getResources().getResourceEntryName(button.getId()).charAt(1) - '0';
-                    // categoria de la posicion de la casilla
-                    String cCasilla = String.valueOf(button.getResources().getResourceEntryName(button.getId()).charAt(0));
-                    // direccion de la casilla
-                    String dCasilla = String.valueOf(button.getResources().getResourceEntryName(button.getId()).charAt(2));
-                    // posicion en la que esta la ficha
-                    int posFicha = b_ficha.getTag().toString().charAt(1) - '0';
-                    // categoria en la que esta la ficha
-                    String cFicha = String.valueOf(b_ficha.getTag().toString().charAt(0));
+                }else if(tirar == 2){
+                    t_j1.setTextColor(Color.RED);
+                    t_j2.setTextColor(Color.GREEN);
+                    tirar++;
 
-                    if(posFicha == 6 && cCasilla.equals("a")){
-                        q_azul.setVisibility(View.VISIBLE);
+                    if (n > nDado) {
+                        turno = 1;
+                        Toast.makeText(getApplicationContext(), "Compienza el jugador " + turno, Toast.LENGTH_SHORT).show();
+                    }else {
+                        turno = 2;
+                        Toast.makeText(getApplicationContext(), "Compienza el jugador " + turno, Toast.LENGTH_SHORT).show();
                     }
 
-                    if (posFicha + nDado <=6) {
+                }else{
 
-                        if (posCasilla  > posFicha) {
+                    for(int id : CASILLAS) {
+                        final Button button = (Button) findViewById(id);
+
+                        // numero de la posicion de la casilla
+                        int posCasilla = button.getResources().getResourceEntryName(button.getId()).charAt(1) - '0';
+                        // categoria de la posicion de la casilla
+                        String cCasilla = String.valueOf(button.getResources().getResourceEntryName(button.getId()).charAt(0));
+                        // direccion de la casilla
+                        String dCasilla = String.valueOf(button.getResources().getResourceEntryName(button.getId()).charAt(2));
+
+                        if(turno == 1){
+
+                            // posicion en la que esta la ficha
+                            posFicha = b_ficha1.getTag().toString().charAt(1) - '0';
+                            // categoria en la que esta la ficha
+                            cFicha = String.valueOf(b_ficha1.getTag().toString().charAt(0));
+
+                            t_j1.setTextColor(Color.GREEN);
+                            t_j2.setTextColor(Color.RED);
+                            b_ficha1.setEnabled(true);
+                            b_ficha2.setEnabled(false);
+
+                        }else if(turno == 2){
+
+                            // posicion en la que esta la ficha
+                            posFicha = b_ficha2.getTag().toString().charAt(1) - '0';
+                            // categoria en la que esta la ficha
+                            cFicha = String.valueOf(b_ficha2.getTag().toString().charAt(0));
+
+                            t_j1.setTextColor(Color.RED);
+                            t_j2.setTextColor(Color.GREEN);
+                            b_ficha1.setEnabled(false);
+                            b_ficha2.setEnabled(true);
+
+                        }
+
+                        if(posFicha == 6 && cCasilla.equals("a")){
+                            q_azul.setVisibility(View.VISIBLE);
+                        }
+
+                        //Control de moviminetos
+
+                        if (posFicha + nDado <= 9 && posCasilla  > posFicha) {
 
                             if (posCasilla == nDado + posFicha && (cCasilla.equals(cFicha) || cFicha.equals("0"))) {
-
                                 moverFicha(button);
+                            }else {
+                                button.setVisibility(View.INVISIBLE);
+                            }
+                        }else if(posFicha >= 4  &&  posFicha  < 6 && (posFicha == 4 && nDado == 6 || posFicha == 5 && nDado == 5 )){
 
-                            } else {
+                            if (posCasilla == (posFicha+nDado)-1 && ((cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(0))) && (dCasilla.equals("d"))) || (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(1))) && (dCasilla.equals("i"))))){
+                                moverFicha(button);
+                            }else {
+                                button.setVisibility(View.INVISIBLE);
+                            }
+                        }else if ( posFicha == 5 && nDado == 6){
+
+                            if(posCasilla == (posFicha+nDado)-3 && ((cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(0))) && (dCasilla.equals("d"))) || (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(1))) && (dCasilla.equals("i"))))){
+                                moverFicha(button);
+                            }else {
+                                button.setVisibility(View.INVISIBLE);
+                            }
+                        }else if( posFicha == 6 && nDado > 3) {
+
+                            if ((posCasilla == ((posFicha-nDado)+(posFicha+1))) && ((cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(0))) && (dCasilla.equals("d"))) || (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(1)))  && (dCasilla.equals("i"))))) {
+                                moverFicha(button);
+                            }else {
+                                button.setVisibility(View.INVISIBLE);
+                            }
+                        }else if( posFicha > 6 ) {
+
+                            if ((posCasilla == (posFicha-nDado) && cFicha.equals(cCasilla))|| posCasilla == ((posFicha-nDado)+((10-posFicha)+(10-(posFicha+1)))) && (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(1))) && (dCasilla.equals("i")) || (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(1))) && posCasilla == 6 ))) {
+                                moverFicha(button);
+                            }else {
                                 button.setVisibility(View.INVISIBLE);
                             }
                         }
-
-                    }else if(posFicha >= 4  &&  posFicha  < 6 && (posFicha == 4 && nDado == 6 || posFicha == 5 && nDado == 5 )){
-
-                        if (posCasilla == (posFicha+nDado)-1 && ((cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(0))) && (dCasilla.equals("d"))) || (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(1)))  && (dCasilla.equals("i"))))){
-
-                            moverFicha(button);
-
-                        } else {
-                            button.setVisibility(View.INVISIBLE);
-                        }
-
-                    }else if( posFicha == 6 && nDado > 3) {
-
-                        if ((posCasilla == ((posFicha-nDado)+(posFicha+1))) && ((cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(0))) && (dCasilla.equals("d"))) || (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(1)))  && (dCasilla.equals("i"))))) {
-
-                            moverFicha(button);
-
-                        } else {
-                            button.setVisibility(View.INVISIBLE);
-                        }
-
-                    }else if( posFicha > 6 && nDado < 4) {
-
-                        if (posCasilla == (posFicha-nDado)) {
-
-                            moverFicha(button);
-
-                        } else {
-                            button.setVisibility(View.INVISIBLE);
-                        }
-                    }else if( posFicha > 6 ) {
-
-                        //Toast.makeText(getApplicationContext(),"prueba "+ cMapa.get(cFicha),Toast.LENGTH_LONG).show();
-
-                        if (posCasilla == ((posFicha-nDado)+((10-posFicha)+(10-(posFicha+1)))) && (cCasilla.equals(String.valueOf(cMapa.get(cFicha).charAt(0))) && (dCasilla.equals("d")))) {
-
-                            moverFicha(button);
-
-                        } else {
-                            button.setVisibility(View.INVISIBLE);
-                        }
-                    }else{
-
-                        if(posCasilla == nDado+posFicha && cCasilla.equals(cFicha) ){
-
-                            moverFicha(button);
-
-                        }else{
-                            button.setVisibility(View.INVISIBLE);
-                        }
                     }
                 }
+
                 int res = getResources().getIdentifier("dado_"+nDado , "drawable", "com.example.trivial");
                 iv_dado.setImageResource(res);
             }
@@ -162,11 +192,27 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                b_ficha.setTag(String.valueOf(b.getResources().getResourceEntryName(b.getId())));
-                b_ficha.setX(b.getX());
-                b_ficha.setY(b.getY());
+
+                if(turno == 1){
+
+                    b_ficha1.setTag(String.valueOf(b.getResources().getResourceEntryName(b.getId())));
+                    b_ficha1.setX(b.getX());
+                    b_ficha1.setY(b.getY());
+
+                }else if(turno == 2){
+
+                    b_ficha2.setTag(String.valueOf(b.getResources().getResourceEntryName(b.getId())));
+                    b_ficha2.setX(b.getX());
+                    b_ficha2.setY(b.getY());
+                }
                 ocultarCasillas();
+                obtenetPreguntas();
             }
         });
+    }
+
+    private void obtenetPreguntas() {
+        Intent intent = new Intent(this, Quiz.class);
+        startActivity(intent);
     }
 }
